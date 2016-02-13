355367b9ac3f612234351f9784dccd16c2c775a8
Joaquim Serafim <joaquim.serafim@gmail.com>
2016-02-13 22:44:52 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const request = require('superagent')
+
+module.exports = Client
+
+function Client (uri, user, pwd, job, socketConfig) {
+  this._uri     = uri
+  this._user    = user
+  this._pwd     = pwd
+  this._job     = job
+  this._timeout = socketConfig && socketConfig.timeout || 5000
+}
+
+Client.prototype.getStatus = function getStatus (cb) {
+
+  request
+    .get(this._uri + '/job/' + this._job + '/api/json')
+    .timeout(this._timeout)
+    .auth(this._user, this._pwd)
+    .end(handler(cb))
+}
+
+Client.prototype.getResult = function getResult (jobNumber, cb) {
+
+  if (!isInteger(jobNumber)) {
+    cb(new Error('"jobNumber" must be an integer!'))
+  }
+
+  request
+    .get(this._uri + '/job/' + this._job + '/' + jobNumber + '/consoleText')
+    .timeout(this._timeout)
+    .auth(this._user, this._pwd)
+    .end(handler(cb))
+}
+
+Client.prototype.build = function build (parameters, cb) {
+
+  if (!Array.isArray(parameters)) {
+    return cb(new Error('"parameters" must be an array!'))
+  }
+
+  var formData = JSON.stringify({parameter: parameters})
+
+  request
+    .post(this._uri + '/job/' + this._job + '/build')
+    .timeout(this._timeout)
+    .query({delay: '0sec'})
+    .type('form')
+    .send({json: formData})
+    .auth(this._user, this._pwd)
+    .end(handler(cb))
+}
+
+//
+// helpers
+//
+
+function handler (cb) {
+
+  return function handlerFn (err, res) {
+    if (err) {
+      cb(new Error(err.message), err.status, res && res.headers)
+    } else {
+      cb(null, res.status, res.body, res.headers)
+    }
+  }
+}
+
+function isInteger (n) {
+  return Number(n) === n && n % 1 === 0
+}
---
+++ test.js
@@ -0,0 +1,141 @@
+'use strict'
+/*jshint -W030*/
+
+const mocha = require('mocha')
+const nock = require('nock')
+const expect = require('chai').expect
+
+const it = mocha.it
+const describe = mocha.describe
+const before = mocha.before
+const afterEach = mocha.afterEach
+
+const Client = require('./')
+
+const URI     = 'http://misterjenkins.com'
+const jobName = 'REEDNANLUBR'
+const user    = 'Deckard'
+const pwd     = 'B26354'
+
+var scope
+
+describe('jenkins-client', function() {
+
+  before(function(done) {
+    nock.disableNetConnect()
+    done()
+  })
+
+  afterEach(function(done) {
+    if (scope) {
+      expect(scope.isDone()).to.be.true
+    }
+    done()
+  })
+
+  it('should throw an error if a timeout or fail connection happens',
+    function(done) {
+      new Client(URI, user, pwd, jobName, {timeout: 500})
+        .getStatus(function(err, statusCode) {
+          expect(err).to.be.exists
+          expect(statusCode).to.be.undefined
+          done()
+        })
+    }
+  )
+
+  it('should throw the corresponding statusCode when jenkins server ' +
+    'returns an error',
+    function(done) {
+      scope = nock(URI)
+        .get('/job/' + jobName + '/api/json')
+        .reply(500)
+
+      new Client(URI, user, pwd, jobName)
+        .getStatus(function(err, statusCode) {
+          expect(statusCode).to.be.equal(500)
+          done()
+        })
+    }
+  )
+
+  it('should return a 200 when getting the job `status`', function(done) {
+    scope = nock(URI)
+      .get('/job/' + jobName + '/api/json')
+      .reply(200, {})
+
+    new Client(URI, user, pwd, jobName)
+      .getStatus(function(err, statusCode, body) {
+        expect(err).to.be.null
+        expect(statusCode).to.be.equal(200)
+        expect(body).to.be.an.object
+        done()
+      })
+  })
+
+  it('should return a 200 when getting the job `result`', function(done) {
+    scope = nock(URI)
+      .get('/job/' + jobName + '/1/consoleText')
+      .reply(200, {})
+
+    new Client(URI, user, pwd, jobName)
+      .getResult(1, function(err, statusCode, body) {
+        expect(err).to.be.null
+        expect(statusCode).to.be.equal(200)
+        expect(body).to.be.an.object
+        done()
+      })
+  })
+
+  it('should return an error when executing a job `build` with a ' +
+    'bad payload to be send',
+    function(done) {
+      new Client(URI, user, pwd, jobName)
+        .getResult('1', function(err, statusCode, body) {
+          expect(err).to.be.exists
+          expect(err.message).to.be.equal('"jobNumber" must be an integer!')
+          expect(statusCode).to.be.undefined
+          expect(body).to.be.undefined
+          done()
+        })
+    }
+  )
+
+  it('should return a 200 when executing a job `build`', function(done) {
+    var params2send = [
+      {name: 'param1', value: 'value1'},
+      {name: 'param2', value: 'value2'},
+      {name: 'param3', value: 'value3'}
+    ]
+
+    var body = 'json=%7B%22parameter%22%3A%5B%7B%22name%22%3A%22param1%22%2' +
+    'C%22value%22%3A%22value1%22%7D%2C%7B%22name%22%3A%22param2%22%2C%22val' +
+    'ue%22%3A%22value2%22%7D%2C%7B%22name%22%3A%22param3%22%2C%22value%22%3' +
+    'A%22value3%22%7D%5D%7D'
+
+    scope = nock(URI)
+      .post('/job/' + jobName + '/build?delay=0sec', body)
+      .reply(201)
+
+    new Client(URI, user, pwd, jobName)
+      .build(params2send, function(err, statusCode) {
+        expect(err).to.be.null
+        expect(statusCode).to.be.equal(201)
+        done()
+      })
+  })
+
+  it('should return an error when executing a job `build` with a ' +
+    'bad payload to be send',
+    function(done) {
+      new Client(URI, user, pwd, jobName)
+        .build({}, function(err, statusCode, body) {
+          expect(err).to.be.exists
+          expect(err.message).to.be.equal('"parameters" must be an array!')
+          expect(statusCode).to.be.undefined
+          expect(body).to.be.undefined
+          done()
+        })
+    }
+  )
+})
